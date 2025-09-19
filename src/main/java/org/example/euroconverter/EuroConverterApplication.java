8a8b85167be0fdee0c062a6b3d35b0801a2b9fbc
AnnaB <altea200@hotmail.com>
2025-09-19 10:45:40 +0200

    Initial commit

---
+++ src/main/java/org/example/euroconverter/EuroConverterApplication.java
@@ -0,0 +1,13 @@
+package org.example.euroconverter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EuroConverterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EuroConverterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/example/euroconverter/EuroConverterApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.euroconverter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EuroConverterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

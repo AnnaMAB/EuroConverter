39edc46863f545702439085843a915bcb9cb2bdf
AnnaB <altea200@hotmail.com>
2025-09-19 11:26:40 +0200

    port 8082

--- src/main/java/org/example/euroconverter/EuroConverterApplication.java
+++ src/main/java/org/example/euroconverter/EuroConverterApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class EuroConverterApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(EuroConverterApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(EuroConverterApplication.class, args);
+    }
 
 }
---
+++ src/main/java/org/example/euroconverter/controllers/SekToEuroController.java
@@ -0,0 +1,17 @@
+package org.example.euroconverter.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/convert")
+public class SekToEuroController {
+
+    @GetMapping("/sek2euro")
+    public Double getConversion(Double sek) {
+        Double euro = sek * 11.03;
+        return euro;
+    }
+
+}

0e2ddc65bad923ef3857a0c0ff5dff8b6aa251f4
AnnaB <altea200@hotmail.com>
2025-09-21 14:34:42 +0200

    dockerfile

--- src/main/java/org/example/euroconverter/controllers/SekToEuroController.java
+++ src/main/java/org/example/euroconverter/controllers/SekToEuroController.java
@@ -4,14 +4,21 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.io.IOException;
+
 @RestController
 @RequestMapping("/convert")
 public class SekToEuroController {
 
     @GetMapping("/sek2euro")
-    public Double getConversion(Double sek) {
-        Double euro = sek * 0.091;
-        return euro;
+    public double getConversionRate() {
+
+        double min = 0.082;
+        double max = 0.111;
+        double sekToEuroRate = min + (Math.random() * (max - min));
+
+        return sekToEuroRate;
     }
+
 
 }
